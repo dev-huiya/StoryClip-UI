1b721c41105d8239d1bcc7c69ae9d7eed0b87389
HuiYa <k_hawon@naver.com>
2020-12-26 12:52:54 +0900

    로그아웃 API 연동

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -4,12 +4,24 @@ import jwt from "jsonwebtoken";
 import store from "store";
 import _ from "lodash";
 
+import query from "api";
+
 function Page({ ...props }) {
 
     useEffect(()=>{
-        store.clearAll();
-        console.log("logout component: logout success");
-        props.history.push('/login');
+        query({
+            url: "/auth/signout",
+            method: "GET",
+        })
+        .then((res) => {
+            console.log(res);
+        })
+        .finally((e)=>{
+            console.log("finally", e);
+            store.clearAll();
+            console.log("logout component: logout success");
+            props.history.push('/login');
+        })
     }, [])
     
     return null;
